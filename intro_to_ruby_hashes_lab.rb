8a4d57544416e436796ab6d32357b08ff160ced0
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-31 04:20:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  {:railroads => {:pieces => "4",
+  {:railroads => {:pieces => 4,
     :rent_in_dollars => {},
     :names => {}
   }}
@@ -11,7 +11,7 @@ end
 
 
 def monopoly_with_third_tier
-  {:railroads => {:pieces => "4",
+  {:railroads => {:pieces => 4,
     :rent_in_dollars => {:one_piece_owned => "25",
       :two_pieces_owned => "50",
       :three_pieces_owned => "100",
