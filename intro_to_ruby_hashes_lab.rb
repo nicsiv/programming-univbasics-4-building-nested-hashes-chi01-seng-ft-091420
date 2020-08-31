a72b9ead3a06029bac71aa23aad2f3214d81913b
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-31 04:14:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 
 
 def monopoly_with_third_tier
-  {:railroads => {:peices => "4",
+  {:railroads => {:peices => 4,
     :rent_in_dollars => {:one_piece_owned => "25",
       :two_pieces_owned => "50",
       :three_pieces_owned => "100",
