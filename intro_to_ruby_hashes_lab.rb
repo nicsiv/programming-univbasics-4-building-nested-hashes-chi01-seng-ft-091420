6d452281b1fedf9424f06275d6603b5a50eb05ff
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-31 04:25:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
     },
     :names => {:reading_railroad => {}, 
       :pennsylvania_railroad => {},
-      :b_ando_railroad => {},
+      :b_and_o_railroad => {},
       :shortline_railroad => {}
     }}}
 end
@@ -33,7 +33,7 @@ def monopoly_with_fourth_tier
     },
     :names => {:reading_railroad => {:mortgage_value => 100}, 
       :pennsylvania_railroad => {:mortgage_value => 200},
-      :b_ando_railroad => {:mortgage_value => 400},
+      :b_and_o_railroad => {:mortgage_value => 400},
       :shortline_railroad => {:mortgage_value => 800}
     }}}
 
