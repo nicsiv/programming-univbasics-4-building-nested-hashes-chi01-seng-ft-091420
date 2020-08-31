75328612902ebfbca467f0cdb2626fd7b5c8a427
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-31 04:13:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
       :two_pieces_owned => "50",
       :three_pieces_owned => "100",
       :four_pieces_owned => "200"
-    }},
+    },
     :names => {:reading_railroad => {}, 
       :pensylvania_railroad => {},
       :b_ando_railroad => {},
